5a10541756ede5c461f7e4d04cb52e8ca08805fd
Lucien Thomas <lthomas@sciport.org>
2024-04-21 10:37:27 -0500

    fix: fix expo router to work correctly

---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, SafeAreaView } from "react-native";
+
+export default function Index() {
+  return (
+    <SafeAreaView className="flex-1 justify-center items-center">
+      <Text className="text-2xl text-green-700">Index</Text>
+    </SafeAreaView>
+  );
+}
