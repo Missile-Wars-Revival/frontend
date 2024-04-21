2203b0c1e7b6815a6d4fa06fb8838e81b864b771
Long Time No C <85569855+longtimeno-c@users.noreply.github.com>
2024-04-21 21:20:08 +0100

    geolocation services e.g. user location

--- app/pages/Map.jsx
+++ app/pages/Map.jsx
@@ -1,36 +1,63 @@
-import React from "react";
-import { Text, SafeAreaView, StyleSheet } from "react-native";
-import MapView from "react-native-maps";
+import React, { useEffect, useState, useCallback } from "react";
+import { Text, View, StyleSheet } from "react-native";
+import MapView, { PROVIDER_GOOGLE } from "react-native-maps";
+import * as Location from 'expo-location';
 
 export default function Map() {
+  const defaultRegion = {
+    latitude: 0,
+    longitude: 0,
+    latitudeDelta: 0.0922,
+    longitudeDelta: 0.0421,
+  };
+
+  const [region, setRegion] = useState(defaultRegion);
+
+  const fetchLocation = useCallback(async () => {
+    let { status } = await Location.requestForegroundPermissionsAsync();
+    if (status !== 'granted') {
+      console.log('Permission to access location was denied');
+      return;
+    }
+
+    let location = await Location.getCurrentPositionAsync({});
+    setRegion({
+      latitude: location.coords.latitude,
+      longitude: location.coords.longitude,
+      latitudeDelta: 0.0922,
+      longitudeDelta: 0.0421,
+    });
+  }, []);
+
+  useEffect(() => {
+    fetchLocation();
+  }, [fetchLocation]);
+
   return (
-    <SafeAreaView style={styles.container}> 
-      <Text style={styles.header}>Map Page</Text> 
+    <View style={styles.container}>
+      <Text style={styles.header}>Map Page- Temp</Text>
       <MapView
-        provider={MapView.PROVIDER_GOOGLE} // Use Google Maps API key can be foud in app.json
+        provider={PROVIDER_GOOGLE}
         style={styles.map}
-        initialRegion={{
-          latitude: 37.78825, // Example latitude
-          longitude: -122.4324, // Example longitude
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421,
-        }}
-        //player data is handled later on - this is just temp map
-      >
-        {/* You can add markers, polygons, etc. here */}
-      </MapView>
-    </SafeAreaView>
+        region={region}
+        showsUserLocation={true}
+        showsMyLocationButton={true}
+      />
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: '#f4f4f4',
   },
-  header: { //Heading style
+  header: {
     fontSize: 24,
-    color: 'green',
-    textAlign: 'center',
+    color: "#333",
+    textAlign: "center",
+    paddingTop: 20,
+    paddingBottom: 10,
   },
   map: {
     flex: 1,
