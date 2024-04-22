b33682133599e7e89569478c8a86329eb369b67c
Long Time No C <85569855+longtimeno-c@users.noreply.github.com>
2024-04-22 07:12:15 +0100

    update: added UIthemes and user location

--- app/pages/Map.jsx
+++ app/pages/Map.jsx
@@ -1,17 +1,268 @@
 import React, { useEffect, useState, useCallback } from "react";
-import { Text, View, StyleSheet } from "react-native";
+import { Text, View, StyleSheet, TouchableOpacity, Modal } from "react-native";
 import MapView, { PROVIDER_GOOGLE } from "react-native-maps";
 import * as Location from 'expo-location';
+
+// Map styles
+const DefaultMapStyle = [
+  {
+    "elementType": "geometry",
+    "stylers": [
+      { "color": "#f5f5f5" }
+    ]
+  },
+  {
+    "elementType": "labels.icon",
+    "stylers": [
+      { "visibility": "off" }
+    ]
+  },
+  {
+    "elementType": "labels.text.fill",
+    "stylers": [
+      { "color": "#616161" }
+    ]
+  },
+  {
+    "elementType": "labels.text.stroke",
+    "stylers": [
+      { "color": "#f5f5f5" }
+    ]
+  },
+  {
+    "featureType": "administrative",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      { "color": "#757575" }
+    ]
+  },
+  {
+    "featureType": "poi",
+    "stylers": [
+      { "visibility": "off" }
+    ]
+  },
+  {
+    "featureType": "road",
+    "elementType": "geometry",
+    "stylers": [
+      { "color": "#ffffff" }
+    ]
+  },
+  {
+    "featureType": "road",
+    "elementType": "labels.icon",
+    "stylers": [
+      { "visibility": "off" }
+    ]
+  },
+  {
+    "featureType": "road.arterial",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      { "color": "#757575" }
+    ]
+  },
+  {
+    "featureType": "road.highway",
+    "elementType": "geometry",
+    "stylers": [
+      { "color": "#dadada" }
+    ]
+  },
+  {
+    "featureType": "road.highway",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      { "color": "#616161" }
+    ]
+  },
+  {
+    "featureType": "water",
+    "elementType": "geometry",
+    "stylers": [
+      { "color": "#0077be" } // Ocean blue color
+    ]
+  },
+  {
+    "featureType": "water",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      { "color": "#9e9e9e" }
+    ]
+  }
+];
+
+const RadarMapStyle = [
+  {
+    "elementType": "geometry",
+    "stylers": [
+      { "color": "#212121" }
+    ]
+  },
+  {
+    "elementType": "labels.icon",
+    "stylers": [
+      { "visibility": "off" }
+    ]
+  },
+  {
+    "elementType": "labels.text.fill",
+    "stylers": [
+      { "color": "#757575" }
+    ]
+  },
+  {
+    "elementType": "labels.text.stroke",
+    "stylers": [
+      { "color": "#212121" }
+    ]
+  },
+  {
+    "featureType": "administrative",
+    "elementType": "geometry",
+    "stylers": [
+      { "color": "#757575" }
+    ]
+  },
+  {
+    "featureType": "administrative.country",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      { "color": "#9e9e9e" }
+    ]
+  },
+  {
+    "featureType": "administrative.locality",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      { "color": "#bdbdbd" }
+    ]
+  },
+  {
+    "featureType": "poi",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      { "color": "#757575" }
+    ]
+  },
+  {
+    "featureType": "poi.park",
+    "elementType": "geometry",
+    "stylers": [
+      { "color": "#181818" }
+    ]
+  },
+  {
+    "featureType": "poi.park",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      { "color": "#616161" }
+    ]
+  },
+  {
+    "featureType": "road",
+    "elementType": "geometry.fill",
+    "stylers": [
+      { "color": "#2c2c2c" }
+    ]
+  },
+  {
+    "featureType": "road",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      { "color": "#8a8a8a" }
+    ]
+  },
+  {
+    "featureType": "road.arterial",
+    "elementType": "geometry",
+    "stylers": [
+      { "color": "#373737" }
+    ]
+  },
+  {
+    "featureType": "road.highway",
+    "elementType": "geometry",
+    "stylers": [
+      { "color": "#3c3c3c" }
+    ]
+  },
+  {
+    "featureType": "road.highway.controlled_access",
+    "elementType": "geometry",
+    "stylers": [
+      { "color": "#4e4e4e" }
+    ]
+  },
+  {
+    "featureType": "road.local",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      { "color": "#616161" }
+    ]
+  },
+  {
+    "featureType": "transit",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      { "color": "#757575" }
+    ]
+  },
+  {
+    "featureType": "water",
+    "elementType": "geometry",
+    "stylers": [
+      { "color": "#000000" }
+    ]
+  },
+  {
+    "featureType": "water",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      { "color": "#3d3d3d" }
+    ]
+  }
+];
+
+
+const MapStylePopup = ({ visible, onClose, onSelect }) => {
+  return (
+    <Modal
+      animationType="slide"
+      transparent={true}
+      visible={visible}
+      onRequestClose={onClose}
+    >
+      <TouchableOpacity
+        style={styles.centeredView}
+        activeOpacity={1}
+        onPressOut={onClose}
+      >
+        <View style={styles.modalView}>
+          <TouchableOpacity onPress={() => onSelect('default')} style={styles.button}>
+            <Text style={styles.buttonText}>Default</Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => onSelect('radar')} style={styles.button}>
+            <Text style={styles.buttonText}>Radar</Text>
+          </TouchableOpacity>
+        </View>
+      </TouchableOpacity>
+    </Modal>
+  );
+};
 
 export default function Map() {
   const defaultRegion = {
     latitude: 0,
     longitude: 0,
-    latitudeDelta: 0.0922,
-    longitudeDelta: 0.0421,
+    latitudeDelta: 90,
+    longitudeDelta: 90,
   };
 
   const [region, setRegion] = useState(defaultRegion);
+  const [selectedMapStyle, setSelectedMapStyle] = useState(DefaultMapStyle); // Default to radar style
+  const [popupVisible, setPopupVisible] = useState(false);
 
   const fetchLocation = useCallback(async () => {
     let { status } = await Location.requestForegroundPermissionsAsync();
@@ -33,15 +284,50 @@ export default function Map() {
     fetchLocation();
   }, [fetchLocation]);
 
+  const showPopup = () => {
+    setPopupVisible(true);
+  };
+
+  const closePopup = () => {
+    setPopupVisible(false);
+  };
+
+  const selectMapStyle = (style) => {
+    closePopup();
+    switch (style) {
+      case 'default':
+        setSelectedMapStyle(DefaultMapStyle);
+        break;
+      case 'radar':
+        setSelectedMapStyle(RadarMapStyle);
+        break;
+      default:
+        break;
+    }
+  };
+
   return (
     <View style={styles.container}>
       <Text style={styles.header}>Map Page- Temp</Text>
+
       <MapView
         provider={PROVIDER_GOOGLE}
         style={styles.map}
         region={region}
         showsUserLocation={true}
         showsMyLocationButton={true}
+        customMapStyle={selectedMapStyle}
+      />
+
+      {/* Dropdown button */}
+      <TouchableOpacity style={styles.dropdownButton} onPress={showPopup}>
+        <Text style={styles.dropdownButtonText}>Style</Text>
+      </TouchableOpacity>
+
+      <MapStylePopup
+        visible={popupVisible}
+        onClose={closePopup}
+        onSelect={selectMapStyle}
       />
     </View>
   );
@@ -61,5 +347,52 @@ const styles = StyleSheet.create({
   },
   map: {
     flex: 1,
+  },
+  dropdownButton: {
+    position: 'absolute',
+    bottom: 20,
+    left: 20,
+    borderRadius: 5,
+    backgroundColor: '#ffffff',
+    elevation: 3,
+    shadowColor: '#000000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.2,
+    shadowRadius: 2,
+    padding: 10,
+  },
+  dropdownButtonText: {
+    fontSize: 16,
+  },
+  centeredView: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0,0,0,0.5)',
+  },
+  modalView: {
+    backgroundColor: 'white',
+    borderRadius: 10,
+    padding: 20,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  button: {
+    borderRadius: 5,
+    padding: 10,
+    marginVertical: 5,
+    backgroundColor: '#f4f4f4',
+    width: 200,
+    alignItems: 'center',
+  },
+  buttonText: {
+    fontSize: 16,
   },
 });
