8132ab46873009218e7d148efc44f018348e0a3b
Lucien Thomas <lthomas@sciport.org>
2024-04-23 15:53:18 -0500

    refactor: route to index on successful login

--- app/login.tsx
+++ app/login.tsx
@@ -1,4 +1,5 @@
 import { SafeAreaView, Text, Button, View } from "react-native";
+import { router } from "expo-router";
 import { Input } from "../components/input";
 import { useState } from "react";
 import axios from "axios";
@@ -7,31 +8,30 @@ export default function Login() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isError, setIsError] = useState(false);
 
   function handleLogin(username: string, password: string) {
     axios
-      .post("http://192.168.1.75:3001/api/login", {
+      .post("http://192.168.1.168:3000/api/login", {
         username,
         password,
       })
       .then((response) => {
         console.log(response.data);
         if (response.status === 200) {
-          setIsLoggedIn(true);
+          router.navigate("/");
         }
       })
       .catch((error) => {
         console.log(error);
+        if (error.response.status === 401) {
+          setIsError(true);
+        }
       });
   }
 
   return (
     <SafeAreaView className="flex-1 justify-center items-center space-y-8">
-      {isLoggedIn ? (
-        <Text className="text-green-700 text-2xl">Logged In!</Text>
-      ) : (
-        <Text className="text-red-700 text-2xl">Not Logged In</Text>
-      )}
       <View className="space-y-4">
         <Input
           placeholder="Username"
@@ -46,6 +46,11 @@ export default function Login() {
       </View>
       <Button title="Login!" onPress={() => handleLogin(username, password)} />
       <Button title="Logout!" onPress={() => setIsLoggedIn(false)} />
+      {isError && (
+        <Text className={"text-red-700 text-lg"}>
+          Invalid username or password
+        </Text>
+      )}
     </SafeAreaView>
   );
 }
