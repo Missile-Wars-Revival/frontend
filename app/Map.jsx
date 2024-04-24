77468c3583e3ee9d3a9934abbf90aa02db6de209
Lucien Thomas <143671731+ljt019@users.noreply.github.com>
2024-04-23 21:15:15 -0500

    feat: add login page and input components (#8)
    * geolocation services e.g. user location
    
    * feat: add login page and input components
    
    - add axios
    - add clsx (merge classnames)
    - create login page
    - create input component
    - add login page to router
    - hook up input and login to backend endpoint
    
    * Create CONTRIBUTING.md draft
    
    * Create CODE_OF_CONDUCT.md
    
    * update: added UIthemes and user location
    
    * update
    
    * added basic logic for loot drops and missiles radius
    
    * Corrected radius for loot drops and missiles
    
    * updated radius to refresh every 60secs
    
    * added alert to say when you are in missiles path and when you collect loot
    
    * Organisation: Moved API into .env, seperated Themes from Map file, created styles.js to centralise styles (followed comments in PR)
    
    * added index page to resolve expo-router issues
    
    * renamed index 'Home Page' cant seem to change header name using expo-router
    
    * added functions for sending and fetching player locaitons
    
    * updated
    
    * handled seeing your own location
    
    * Update CODE_OF_CONDUCT.md
    
    * added temp variable for username to make it easier when Core-UI is done to attach onto this
    
    * refactor: route to index on successful login
    
    * fix: update ts-config to use expo jsx
    
    * feat: add login page and input components
    
    - add axios
    - add clsx (merge classnames)
    - create login page
    - create input component
    - add login page to router
    - hook up input and login to backend endpoint
    
    * refactor: route to index on successful login
    
    * fix: update ts-config to use expo jsx
    
    * fix: remove random login.js file
    
    * fix: login.js needed for map page
    
    I renamed login.js to login.ts and moved it into the temp folder
    
    * dev: moved backend url to .env
    
    --moved temp folder out of app folder, should only be used for routes
    
    ---------
    
    Co-authored-by: Long Time No C <85569855+longtimeno-c@users.noreply.github.com>
    Co-authored-by: Kevin Gray <graykevinb@gmail.com>

--- app/Map.jsx
+++ app/Map.jsx
@@ -1,18 +1,18 @@
 import React, { useEffect, useState, useCallback } from "react";
 import { Text, View, StyleSheet, TouchableOpacity, Modal } from "react-native";
 import MapView, { PROVIDER_GOOGLE, Circle } from "react-native-maps";
-import * as Location from 'expo-location';
+import * as Location from "expo-location";
 
 //Themes
-import { DefaultMapStyle } from './Themes/DefaultMapStyle';
-import { RadarMapStyle } from './Themes/RadarMapStyle';
-import { CherryBlossomMapStyle } from './Themes/CherryBlossomMapStyle';
+import { DefaultMapStyle } from "./Themes/DefaultMapStyle";
+import { RadarMapStyle } from "./Themes/RadarMapStyle";
+import { CherryBlossomMapStyle } from "./Themes/CherryBlossomMapStyle";
 
 //Stylesheet
-import { styles } from './styles';
+import { styles } from "./styles";
 
 //import username
-import { userNAME } from './login.js';
+import { userNAME } from "../temp/login";
 
 // Loot Component
 const Loot = ({ location }) => (
@@ -48,13 +48,22 @@ const MapStylePopup = ({ visible, onClose, onSelect }) => {
         onPressOut={onClose}
       >
         <View style={styles.modalView}>
-          <TouchableOpacity onPress={() => onSelect('default')} style={styles.button}>
+          <TouchableOpacity
+            onPress={() => onSelect("default")}
+            style={styles.button}
+          >
             <Text style={styles.buttonText}>Default</Text>
           </TouchableOpacity>
-          <TouchableOpacity onPress={() => onSelect('radar')} style={styles.button}>
+          <TouchableOpacity
+            onPress={() => onSelect("radar")}
+            style={styles.button}
+          >
             <Text style={styles.buttonText}>Radar</Text>
           </TouchableOpacity>
-          <TouchableOpacity onPress={() => onSelect('cherry')} style={styles.button}>
+          <TouchableOpacity
+            onPress={() => onSelect("cherry")}
+            style={styles.button}
+          >
             <Text style={styles.buttonText}>Cherry Blossom</Text>
           </TouchableOpacity>
         </View>
@@ -64,14 +73,13 @@ const MapStylePopup = ({ visible, onClose, onSelect }) => {
 };
 
 export default function Map() {
-
   const defaultRegion = {
     latitude: 0,
     longitude: 0,
     latitudeDelta: 90,
     longitudeDelta: 90,
   };
-  
+
   const [region, setRegion] = useState(defaultRegion);
   const [selectedMapStyle, setSelectedMapStyle] = useState(DefaultMapStyle);
   const [popupVisible, setPopupVisible] = useState(false);
@@ -90,14 +98,13 @@ export default function Map() {
     fetchOtherPlayers();
   }, []);
 
-
   const fetchLocation = useCallback(async () => {
     let { status } = await Location.requestForegroundPermissionsAsync();
-    if (status !== 'granted') {
-      console.log('Permission to access location was denied');
+    if (status !== "granted") {
+      console.log("Permission to access location was denied");
       return;
     }
-  
+
     let location = await Location.getCurrentPositionAsync({});
     const userLoc = {
       latitude: location.coords.latitude,
@@ -112,67 +119,72 @@ export default function Map() {
     setUserLocation(userLoc);
   }, []);
 
-const apiUrl = 'http://172.20.10.5:3000/api/';
+  const apiUrl = "http://172.20.10.5:3000/api/";
 
-const fetchData = async (endpoint, method = 'GET', data = null) => {
-  const config = {
-    method: method,
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  };
+  const fetchData = async (endpoint, method = "GET", data = null) => {
+    const config = {
+      method: method,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
 
-  if (data) {
-    config.body = JSON.stringify(data);
-  }
-
-  try {
-    const response = await fetch(apiUrl + endpoint, config);
-
-    if (!response.ok) {
-      throw new Error(`Failed to fetch data. Status: ${response.status}`);
+    if (data) {
+      config.body = JSON.stringify(data);
     }
 
-    return await response.json();
-  } catch (error) {
-    console.error(`Error fetching data: ${error.message}`);
-    throw error;
-  }
-};
+    try {
+      const response = await fetch(apiUrl + endpoint, config);
 
-const sendLocationToBackend = async (latitude, longitude) => {
-  try {
-    const data = await fetchData('sendLocation', 'POST', {
-      username: userNAME,
-      latitude,
-      longitude,
-    });
-    console.log('Location sent successfully:', data);
-  } catch (error) {
-    console.error('Error sending location to backend:', error.message);
-  }
-};
+      if (!response.ok) {
+        throw new Error(`Failed to fetch data. Status: ${response.status}`);
+      }
 
-const fetchOtherPlayersData = async () => {
-  try {
-    const data = await fetchData('getOtherPlayersData');
-    console.log('Other players data fetched successfully:', data);
-    
-    // Filter out players with the same username
-    const filteredData = data.filter(player => player.username !== userNAME); // Replace 'test' with your username
-    
-    return filteredData;
-  } catch (error) {
-    console.error('Error fetching other players data:', error.message);
-    return [];
-  }
-};
+      return await response.json();
+    } catch (error) {
+      console.error(`Error fetching data: ${error.message}`);
+      throw error;
+    }
+  };
 
+  const sendLocationToBackend = async (latitude, longitude) => {
+    try {
+      const data = await fetchData("sendLocation", "POST", {
+        username: userNAME,
+        latitude,
+        longitude,
+      });
+      console.log("Location sent successfully:", data);
+    } catch (error) {
+      console.error("Error sending location to backend:", error.message);
+    }
+  };
 
-  
+  const fetchOtherPlayersData = async () => {
+    try {
+      const data = await fetchData("getOtherPlayersData");
+      console.log("Other players data fetched successfully:", data);
+
+      // Filter out players with the same username
+      const filteredData = data.filter(
+        (player) => player.username !== userNAME
+      ); // Replace 'test' with your username
+
+      return filteredData;
+    } catch (error) {
+      console.error("Error fetching other players data:", error.message);
+      return [];
+    }
+  };
+
   const checkMissileCollision = () => {
     for (let missile of missileData) {
-      const distance = getDistance(userLocation.latitude, userLocation.longitude, missile.location.latitude, missile.location.longitude);
+      const distance = getDistance(
+        userLocation.latitude,
+        userLocation.longitude,
+        missile.location.latitude,
+        missile.location.longitude
+      );
       if (distance <= missile.radius) {
         alert("Warning: You are in the radius of a missile!");
         console.log("Player died");
@@ -180,31 +192,39 @@ const fetchOtherPlayersData = async () => {
       }
     }
   };
-  
+
   const checkLootCollection = () => {
     for (let loot of lootLocations) {
-      const distance = getDistance(userLocation.latitude, userLocation.longitude, loot.latitude, loot.longitude);
-      if (distance <= 40) { // Assuming loot radius is 40 meters
+      const distance = getDistance(
+        userLocation.latitude,
+        userLocation.longitude,
+        loot.latitude,
+        loot.longitude
+      );
+      if (distance <= 40) {
+        // Assuming loot radius is 40 meters
         alert("You've found loot nearby!");
         console.log("Loot collected");
         break;
       }
     }
   };
-  
+
   const getDistance = (lat1, lon1, lat2, lon2) => {
     const R = 6371; // Radius of the earth in km
     const dLat = deg2rad(lat2 - lat1);
     const dLon = deg2rad(lon2 - lon1);
     const a =
       Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-      Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) *
-      Math.sin(dLon / 2) * Math.sin(dLon / 2);
+      Math.cos(deg2rad(lat1)) *
+        Math.cos(deg2rad(lat2)) *
+        Math.sin(dLon / 2) *
+        Math.sin(dLon / 2);
     const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
     const d = R * c; // Distance in km
     return d * 1000; // Distance in meters
   };
-  
+
   const deg2rad = (deg) => {
     return deg * (Math.PI / 180);
   };
@@ -216,7 +236,6 @@ const fetchOtherPlayersData = async () => {
       sendLocationToBackend(userLocation.latitude, userLocation.longitude); // Send location to backend
     }
   }, [userLocation]);
-  
 
   const fetchLootAndMissiles = useCallback(() => {
     // Fetch loot and missile data from backend
@@ -224,32 +243,35 @@ const fetchOtherPlayersData = async () => {
       // Simulated fetch function to get loot data:
       return [
         { latitude: 51.026281, longitude: -3.113764 }, // Loot location 1 TS
-        { latitude: 45.305, longitude: -0.860 }, // Loot location 2
+        { latitude: 45.305, longitude: -0.86 }, // Loot location 2
       ];
     };
-  
+
     const fetchMissilesFromBackend = async () => {
       // Simulated fetch function to get missile data:
       return [
-        { location: { latitude: 45.2949318, longitude: -0.852764 }, radius: 100 }, //temp missile locaiton 
+        {
+          location: { latitude: 45.2949318, longitude: -0.852764 },
+          radius: 100,
+        }, //temp missile locaiton
         { location: { latitude: 51.025316, longitude: -3.115612 }, radius: 50 }, //2nd temp missle location TS
       ];
     };
-  
+
     const updateData = async () => {
       const lootData = await fetchLootFromBackend();
       const missileData = await fetchMissilesFromBackend();
-  
+
       setLootLocations(lootData);
       setMissileData(missileData);
     };
-  
+
     // Initial fetch
     updateData();
-  
+
     // Fetch data every 60 seconds
     const intervalId = setInterval(updateData, 60000); // 60 seconds
-  
+
     // Cleanup interval on component unmount
     return () => {
       clearInterval(intervalId);
@@ -272,68 +294,66 @@ const fetchOtherPlayersData = async () => {
   const selectMapStyle = (style) => {
     closePopup();
     switch (style) {
-      case 'default':
+      case "default":
         setSelectedMapStyle(DefaultMapStyle);
         break;
-      case 'radar':
+      case "radar":
         setSelectedMapStyle(RadarMapStyle);
         break;
-      case 'cherry':
+      case "cherry":
         setSelectedMapStyle(CherryBlossomMapStyle);
         break;
       default:
         break;
     }
   };
-//logs for location 
-//console.log("lootLocations:", lootLocations);
-//console.log("missileData:", missileData);
+  //logs for location
+  //console.log("lootLocations:", lootLocations);
+  //console.log("missileData:", missileData);
 
   return (
     <View style={styles.container}>
-
       <MapView
-  provider={PROVIDER_GOOGLE}
-  style={styles.map}
-  region={region}
-  showsUserLocation={true}
-  showsMyLocationButton={true}
-  customMapStyle={selectedMapStyle}
->
-  {/* Render Loot Drops */}
-  {lootLocations.map((location, index) => (
-    <Circle
-    key={index}
-    center={location}
-    radius={20} //actual radius size
-    fillColor="rgba(0, 0, 255, 0.5)"
-    strokeColor="rgba(0, 0, 255, 0.8)"
-  />  
-  ))}
+        provider={PROVIDER_GOOGLE}
+        style={styles.map}
+        region={region}
+        showsUserLocation={true}
+        showsMyLocationButton={true}
+        customMapStyle={selectedMapStyle}
+      >
+        {/* Render Loot Drops */}
+        {lootLocations.map((location, index) => (
+          <Circle
+            key={index}
+            center={location}
+            radius={20} //actual radius size
+            fillColor="rgba(0, 0, 255, 0.5)"
+            strokeColor="rgba(0, 0, 255, 0.8)"
+          />
+        ))}
 
-  {/* Render Missiles */}
-  {missileData.map(({ location, radius }, index) => (
-    <Circle
-      key={index}
-      center={location}
-      radius={radius}
-      fillColor="rgba(255, 0, 0, 0.5)"
-      strokeColor="rgba(255, 0, 0, 0.8)"
-    />
-  ))}
+        {/* Render Missiles */}
+        {missileData.map(({ location, radius }, index) => (
+          <Circle
+            key={index}
+            center={location}
+            radius={radius}
+            fillColor="rgba(255, 0, 0, 0.5)"
+            strokeColor="rgba(255, 0, 0, 0.8)"
+          />
+        ))}
 
-  {/* Render Other Players */}
-  {otherPlayersData.map((player, index) => (
-        <Circle
-          key={index}
-          center={{ latitude: player.latitude, longitude: player.longitude }}
-          radius={5} // Assuming a radius for other players
-          fillColor="rgba(0, 255, 0, 0.5)" // Green color
-          strokeColor="rgba(0, 255, 0, 0.8)"
-        />
-      ))}
-  
-</MapView>
+        {/* Render Other Players */}
+        {otherPlayersData.map((player, index) => (
+          <Circle
+            key={index}
+            center={{ latitude: player.latitude, longitude: player.longitude }}
+            radius={5} // Assuming a radius for other players
+            fillColor="rgba(0, 255, 0, 0.5)" // Green color
+            strokeColor="rgba(0, 255, 0, 0.8)"
+          />
+        ))}
+      </MapView>
 
       {/* Dropdown button */}
       <TouchableOpacity style={styles.dropdownButton} onPress={showPopup}>
