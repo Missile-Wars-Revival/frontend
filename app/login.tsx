203e822fac2520bc4eccf2be56827ebf90359603
Lucien Thomas <143671731+ljt019@users.noreply.github.com>
2024-04-23 21:15:15 -0500

    feat: add login page and input components (#8)
    * geolocation services e.g. user location
    
    * feat: add login page and input components
    
    - add axios
    - add clsx (merge classnames)
    - create login page
    - create input component
    - add login page to router
    - hook up input and login to backend endpoint
    
    * Create CONTRIBUTING.md draft
    
    * Create CODE_OF_CONDUCT.md
    
    * update: added UIthemes and user location
    
    * update
    
    * added basic logic for loot drops and missiles radius
    
    * Corrected radius for loot drops and missiles
    
    * updated radius to refresh every 60secs
    
    * added alert to say when you are in missiles path and when you collect loot
    
    * Organisation: Moved API into .env, seperated Themes from Map file, created styles.js to centralise styles (followed comments in PR)
    
    * added index page to resolve expo-router issues
    
    * renamed index 'Home Page' cant seem to change header name using expo-router
    
    * added functions for sending and fetching player locaitons
    
    * updated
    
    * handled seeing your own location
    
    * Update CODE_OF_CONDUCT.md
    
    * added temp variable for username to make it easier when Core-UI is done to attach onto this
    
    * refactor: route to index on successful login
    
    * fix: update ts-config to use expo jsx
    
    * feat: add login page and input components
    
    - add axios
    - add clsx (merge classnames)
    - create login page
    - create input component
    - add login page to router
    - hook up input and login to backend endpoint
    
    * refactor: route to index on successful login
    
    * fix: update ts-config to use expo jsx
    
    * fix: remove random login.js file
    
    * fix: login.js needed for map page
    
    I renamed login.js to login.ts and moved it into the temp folder
    
    * dev: moved backend url to .env
    
    --moved temp folder out of app folder, should only be used for routes
    
    ---------
    
    Co-authored-by: Long Time No C <85569855+longtimeno-c@users.noreply.github.com>
    Co-authored-by: Kevin Gray <graykevinb@gmail.com>

---
+++ app/login.tsx
@@ -0,0 +1,58 @@
+import { SafeAreaView, Text, Button, View } from "react-native";
+import { router } from "expo-router";
+import { Input } from "../components/input";
+import { useState } from "react";
+import axios from "axios";
+import Constants from "expo-constants";
+
+export default function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [isError, setIsError] = useState(false);
+
+  const backendUrl = process.env.EXPO_PUBLIC_BACKEND_URL;
+  console.log(backendUrl);
+
+  function handleLogin(username: string, password: string) {
+    axios
+      .post(`${backendUrl}:3000/api/login`, {
+        username,
+        password,
+      })
+      .then((response) => {
+        console.log(response.data);
+        if (response.status === 200) {
+          router.navigate("/");
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+        if (error.response.status === 401) {
+          setIsError(true);
+        }
+      });
+  }
+
+  return (
+    <SafeAreaView className="flex-1 justify-center items-center space-y-8">
+      <View className="space-y-4">
+        <Input
+          placeholder="Username"
+          onChangeText={(text) => setUsername(text)}
+          className="w-[50vw]"
+        />
+        <Input
+          placeholder="Password"
+          onChangeText={(text) => setPassword(text)}
+          className="w-[50vw]"
+        />
+      </View>
+      <Button title="Login!" onPress={() => handleLogin(username, password)} />
+      {isError && (
+        <Text className={"text-red-700 text-lg"}>
+          Invalid username or password
+        </Text>
+      )}
+    </SafeAreaView>
+  );
+}
