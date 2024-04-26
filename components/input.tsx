d05ae52e95479a90b3659063b86b179ca07291ad
Lucien Thomas <lthomas@sciport.org>
2024-04-26 12:59:02 -0500

    dev: Update to input component, updated styles on login page

--- components/input.tsx
+++ components/input.tsx
@@ -1,23 +1,30 @@
 import * as React from "react";
-import { TextInput } from "react-native";
+import { TextInput, View, TextInputProps } from "react-native";
+import { ReactNode } from "react";
 
-import { cn } from "../lib/cn";
+interface InputProps extends TextInputProps {
+  icon?: ReactNode;
+  className?: string;
+}
 
-const Input = React.forwardRef<
-  React.ElementRef<typeof TextInput>,
-  React.ComponentPropsWithoutRef<typeof TextInput>
->(({ className, ...props }, ref) => {
-  return (
-    <TextInput
-      ref={ref}
-      className={cn(
-        "border-[1.5px] border-black rounded-md p-2 w-full ",
-        className
-      )}
-      {...props}
-    />
-  );
-});
+const Input = React.forwardRef<React.ElementRef<typeof TextInput>, InputProps>(
+  ({ className, icon, ...props }, ref) => {
+    return (
+      <View className={`flex-row items-center ${className}`}>
+        {icon && (
+          <View className="absolute inset-y-0 left-2 flex items-center justify-center">
+            {icon}
+          </View>
+        )}
+        <TextInput
+          ref={ref}
+          className={`border-[1.5px] border-black rounded-md p-2 w-full pl-8 ${className}`}
+          {...props}
+        />
+      </View>
+    );
+  }
+);
 
 Input.displayName = "Input";
 
