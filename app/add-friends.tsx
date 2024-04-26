37dce89c71ec0ae5c63ab15be442187070c80fcc
Lucien Thomas <lthomas@sciport.org>
2024-04-26 08:22:44 -0500

    chore: update add-friends to use nativewind

--- app/add-friends.tsx
+++ app/add-friends.tsx
@@ -1,6 +1,7 @@
-import React, { useState, useEffect, useCallback } from 'react';
-import { Text, View, TextInput, FlatList, TouchableOpacity, StyleSheet, Alert } from 'react-native';
-import * as Location from 'expo-location';
+import React, { useState, useEffect, useCallback } from "react";
+import { Text, View, FlatList, TouchableOpacity, Alert } from "react-native";
+import * as Location from "expo-location";
+import { Input } from "../components/input";
 
 const backendUrl: string = process.env.EXPO_PUBLIC_BACKEND_URL!;
 const apiUrl: string = `${backendUrl}:3000/api/`;
@@ -13,16 +14,19 @@ interface Player {
 }
 
 const QuickAddPage: React.FC = () => {
-  const [userLocation, setUserLocation] = useState<{ latitude: number; longitude: number } | null>(null);
-  const [searchTerm, setSearchTerm] = useState<string>('');
+  const [userLocation, setUserLocation] = useState<{
+    latitude: number;
+    longitude: number;
+  } | null>(null);
+  const [searchTerm, setSearchTerm] = useState<string>("");
   const [playersData, setPlayersData] = useState<Player[]>([]);
   const [loading, setLoading] = useState<boolean>(true);
 
   const fetchLocation = useCallback(async () => {
     try {
       let { status } = await Location.requestForegroundPermissionsAsync();
-      if (status !== 'granted') {
-        console.log('Permission to access location was denied');
+      if (status !== "granted") {
+        console.log("Permission to access location was denied");
         return;
       }
 
@@ -33,26 +37,29 @@ const QuickAddPage: React.FC = () => {
       };
       setUserLocation(userLoc);
     } catch (error) {
-      console.log('Error fetching location:');
+      console.log("Error fetching location:");
     }
   }, []);
 
-  const fetchData = async (endpoint: string, method: string = 'GET', data: any = null) => {
-    const config: { 
-      method: string; 
-      headers: { 'Content-Type': string }; 
-      body?: string | null; 
+  const fetchData = async (
+    endpoint: string,
+    method: string = "GET",
+    data: any = null
+  ) => {
+    const config: {
+      method: string;
+      headers: { "Content-Type": string };
+      body?: string | null;
     } = {
       method: method,
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
     };
 
     if (data) {
-  config.body = JSON.stringify(data as object);
-}
-
+      config.body = JSON.stringify(data as object);
+    }
 
     try {
       const response = await fetch(apiUrl + endpoint, config);
@@ -72,21 +79,21 @@ const QuickAddPage: React.FC = () => {
     try {
       // Check if userLocation is available
       if (!userLocation) {
-        console.log('User location is not available');
+        console.log("User location is not available");
         setLoading(false);
         return;
       }
-  
+
       const { latitude, longitude } = userLocation;
-  
+
       const requestData = {
         username: userNAME, // Assuming userNAME is available from the state or props
         latitude: latitude,
         longitude: longitude,
       };
-  
-      const data = await fetchData('nearby', 'POST', requestData);
-  
+
+      const data = await fetchData("nearby", "POST", requestData);
+
       if (data && data.nearbyUsers) {
         const recentPlayersData = data.nearbyUsers.filter((player: Player) => {
           const playerTime = new Date(player.timestamp).getTime();
@@ -94,160 +101,102 @@ const QuickAddPage: React.FC = () => {
           const twoWeeksInMillis = 2 * 7 * 24 * 60 * 60 * 1000;
           return currentTime - playerTime <= twoWeeksInMillis;
         });
-  
+
         setPlayersData(recentPlayersData);
         setLoading(false);
       } else {
-        console.log('No nearby users found');
+        console.log("No nearby users found");
         setPlayersData([]);
         setLoading(false);
       }
     } catch (error) {
-      console.log('Error fetching other players data:', error);
+      console.log("Error fetching other players data:", error);
       setLoading(false);
     }
-  };  
+  };
 
   useEffect(() => {
     fetchLocation();
   }, []);
-  
+
   useEffect(() => {
     if (userLocation) {
       fetchOtherPlayersData();
     }
-  }, [userLocation]);  
+  }, [userLocation]);
 
   const addFriend = async (friendUsername: string) => {
     try {
-      const data = await fetchData('addFriend', 'POST', { friendUsername });
-      if (data && data.message === 'Friend added') {
-        Alert.alert('Success', 'Friend added successfully!');
+      const data = await fetchData("addFriend", "POST", { friendUsername });
+      if (data && data.message === "Friend added") {
+        Alert.alert("Success", "Friend added successfully!");
       }
     } catch (error) {
-      console.log('Error adding friend:', error);
-      Alert.alert('Error', 'Failed to add friend. Please try again.');
+      console.log("Error adding friend:", error);
+      Alert.alert("Error", "Failed to add friend. Please try again.");
     }
   };
 
   const removeFriend = async (friendUsername: string) => {
     try {
-      const data = await fetchData('removeFriend', 'DELETE', { friendUsername });
-      if (data && data.message === 'Friend removed') {
-        Alert.alert('Success', 'Friend removed successfully!');
+      const data = await fetchData("removeFriend", "DELETE", {
+        friendUsername,
+      });
+      if (data && data.message === "Friend removed") {
+        Alert.alert("Success", "Friend removed successfully!");
       }
     } catch (error) {
-      console.log('Error removing friend:', error);
-      Alert.alert('Error', 'Failed to remove friend. Please try again.');
+      console.log("Error removing friend:", error);
+      Alert.alert("Error", "Failed to remove friend. Please try again.");
     }
   };
 
   const renderItem = ({ item }: { item: Player }) => (
-    <View style={styles.playerItem}>
-      <Text style={styles.playerName}>{item.username}</Text>
-      <View style={styles.actions}>
-        <TouchableOpacity 
-          style={styles.actionButton}
+    <View className="flex-row items-center justify-between mb-[10px]">
+      <Text className="text-[16px] flex-1">{item.username}</Text>
+      <View className="flex-row items-center">
+        <TouchableOpacity
+          className="bg-green-600 p-[10px] rounded-[5px] w-[35px] h-[35px] justify-center items-center mr-[10px]"
           onPress={() => addFriend(item.username)}
         >
-          <Text style={styles.actionButtonText}>+</Text>
+          <Text className="font-[13px] text-white">+</Text>
         </TouchableOpacity>
-        <TouchableOpacity 
-          style={styles.actionButtonRed}
+        <TouchableOpacity
+          className="bg-red-500 p-[10px] rounded-[5px] w-[35px] h-[35px] justify-center items-center mr-[10px]"
           onPress={() => removeFriend(item.username)}
         >
-          <Text style={styles.actionButtonText}>x</Text>
+          <Text className="font-[13px] text-white">x</Text>
         </TouchableOpacity>
       </View>
     </View>
-  );  
+  );
 
   return (
-    <View style={styles.container}>
-      <TextInput
-        style={styles.searchInput}
+    <View className="flex-1 p-[20px]">
+      <Input
+        className="h-[40px] border border-gray-500 mb-[20px] px-[10px]"
         placeholder="Search for friends..."
         value={searchTerm}
         onChangeText={setSearchTerm}
       />
       {loading ? (
         <Text>Loading...</Text>
+      ) : playersData.length === 0 ? (
+        <Text className="text-[16px] text-center">
+          No players found near you
+        </Text>
       ) : (
-        playersData.length === 0 ? (
-          <Text style={styles.noPlayersText}>No players found near you</Text>
-        ) : (
-          <>
-            <Text style={styles.title}>Player's Nearby:</Text>
-            <FlatList
-              data={playersData}
-              renderItem={renderItem}
-              keyExtractor={(item) => item.username}
-            />
-          </>
-        )
+        <>
+          <Text className="text-[20px] pb-[10px]">Player's Nearby:</Text>
+          <FlatList
+            data={playersData}
+            renderItem={renderItem}
+            keyExtractor={(item) => item.username}
+          />
+        </>
       )}
     </View>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 20,
-  },
-  searchInput: {
-    height: 40,
-    borderColor: 'gray',
-    borderWidth: 1,
-    marginBottom: 20,
-    paddingHorizontal: 10,
-  },
-  playerItem: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-between',
-    marginBottom: 10,
-  },
-  playerName: {
-    flex: 1,
-    fontSize: 18,
-  },
-  actions: {
-    flexDirection: 'row',
-    alignItems: 'center',
-  },
-  actionButton: {
-    backgroundColor: '#4CAF50',
-    padding: 10,
-    borderRadius: 5,
-    width: 35,
-    height: 35,
-    justifyContent: 'center',
-    alignItems: 'center',
-    marginRight: 10,
-  },
-  actionButtonRed: {
-    backgroundColor: 'lightred',  // Changed to red color
-    padding: 10,
-    borderRadius: 5,
-    width: 35,
-    height: 35,
-    justifyContent: 'center',
-    alignItems: 'center',
-    marginRight: 10,
-  },
-  actionButtonText: {
-    color: 'white',
-    fontSize: 13,
-  },
-  noPlayersText: {
-    textAlign: 'center',
-    fontSize: 16,
-  },
-  title: {
-    fontSize: 20,
-    marginBottom: 10,
-  },
-});
 
 export default QuickAddPage;
