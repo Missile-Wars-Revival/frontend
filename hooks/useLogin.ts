74faacdb8c47cfef31cc3eb9205c7c22aaf15418
Long Time No C <85569855+longtimeno-c@users.noreply.github.com>
2024-04-27 07:07:58 +0100

    reverted previous commit

--- hooks/useLogin.ts
+++ hooks/useLogin.ts
@@ -1,10 +1,10 @@
-import axios, { AxiosError } from "axios";
+import axios from "axios";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 async function login(username: string, password: string) {
   try {
     const response = await axios.post(
-      `${process.env.EXPO_PUBLIC_BACKEND_URL}:3000/api/login`,
+      `${process.env.EXPO_PUBLIC_BACKEND_URL}/api/login`,
       {
         username,
         password,
@@ -14,47 +14,28 @@ async function login(username: string, password: string) {
     return response.data;
   } catch (error) {
     if (axios.isAxiosError(error)) {
-      const axiosError = error as AxiosError;
-      console.log("Axios error:", axiosError.message);
-      if (axiosError.response) {
-        console.log("Status:", axiosError.response.status);
-        console.log("Data:", axiosError.response.data);
-        return axiosError.response.data;
-      }
-    } else {
-      console.log("Non-Axios error:");
+      return error.response?.data;
     }
-    throw error; // Rethrow the error to handle it in the mutation hook
+
+    console.log(error);
   }
 }
 
-export default function useLogin(onSuccess: () => void) {
+export default function useLogin() {
   const queryClient = useQueryClient();
   return useMutation({
-    mutationFn: async ({
+    mutationFn: ({
       username,
       password,
     }: {
       username: string;
       password: string;
-    }) => {
-      try {
-        const data = await login(username, password);
-        return data;
-      } catch (error) {
-        console.log("Login failed:");
-        throw error; // Rethrow the error to handle it in the mutation hook
-      }
-    },
+    }) => login(username, password),
     onSuccess: () => {
       queryClient.invalidateQueries({
         queryKey: ["currentUser"],
         refetchType: "active",
       });
-      onSuccess();
-    },
-    onError: (error) => {
-      console.log("Mutation error:", error.message);
     },
   });
 }
