69d09ff0b7a429a3bef33fcf00dba1095afcc473
Lucien Thomas <lthomas@sciport.org>
2024-04-28 15:39:53 -0500

    dev: updated ts config some more

--- hooks/useNearby.ts
+++ hooks/useNearby.ts
@@ -1,5 +1,2 @@
 import axios from "axios";
 import { useQuery, useQueryClient } from "@tanstack/react-query";
-
-// Confused about how to do this one, its a get but you are passing in data
-// need to talk to clxde before I can do this one
