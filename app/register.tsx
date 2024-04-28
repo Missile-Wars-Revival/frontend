61bcb9da130583c8609ff8c961630f886ffddd9c
Lucien Thomas <lthomas@sciport.org>
2024-04-28 17:24:22 -0500

    fix: oopsie in resovling merge conflicts

--- app/register.tsx
+++ app/register.tsx
@@ -5,15 +5,7 @@ import {
   TouchableHighlight,
   Image,
 } from "react-native";
-import {
-  SafeAreaView,
-  Text,
-  View,
-  TouchableHighlight,
-  Image,
-} from "react-native";
 import { router } from "expo-router";
-import { Input } from "../components/ui/input";
 import { Input } from "../components/ui/input";
 import { useState } from "react";
 import { User, LockKeyhole, Mail } from "lucide-react-native";
@@ -24,18 +16,8 @@ export default function Register() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-  const mutate = useRegister(() => {
-    router.navigate("/login");
-  });
-
   return (
     <SafeAreaView className="flex-1 justify-center items-center space-y-8">
-      <Image
-        source={require("../assets/MissleWarsTitle.png")}
-        className="w-[425px] h-[200px] absolute top-[25]"
-        resizeMode="contain"
-      />
-      <View className="space-y-4 absolute top-[26%]">
       <Image
         source={require("../assets/MissleWarsTitle.png")}
         className="w-[425px] h-[200px] absolute top-[25]"
@@ -47,18 +29,10 @@ export default function Register() {
           onChangeText={(text) => setUsername(text)}
           className="w-[90vw] h-[5vh] rounded-[20px]"
           icon={<User size={24} color="black" />}
-          className="w-[90vw] h-[5vh] rounded-[20px]"
-          icon={<User size={24} color="black" />}
         />
         <Input
           placeholder="Email"
           onChangeText={(text) => setEmail(text)}
-          className="w-[90vw] h-[5vh] rounded-[20px]"
-          icon={
-            <View className="inset-y-[9px]">
-              <Mail size={24} color="black" />
-            </View>
-          }
           className="w-[90vw] h-[5vh] rounded-[20px]"
           icon={
             <View className="inset-y-[9px]">
@@ -75,15 +49,8 @@ export default function Register() {
               <LockKeyhole size={24} color="black" />
             </View>
           }
-          className="w-[90vw] h-[5vh] rounded-[20px]"
-          icon={
-            <View className="inset-y-[7px]">
-              <LockKeyhole size={24} color="black" />
-            </View>
-          }
         />
       </View>
-      <SignUpButton email={email} username={username} password={password} />
       <SignUpButton email={email} username={username} password={password} />
     </SafeAreaView>
   );
@@ -98,32 +65,7 @@ function SignUpButton({
   username: string;
   password: string;
 }) {
-  const mutation = useRegisterUser(() => {
-    router.navigate("/");
-  });
-
-  return (
-    <TouchableHighlight
-      onPress={() => mutation.mutate({ username, password, email })}
-      className="bg-[#773765] rounded-[20px] w-[375px] h-[45px] flex items-center justify-center absolute top-[60%]"
-    >
-      <View>
-        <Text className="text-white font-bold">Sign Up!</Text>
-      </View>
-    </TouchableHighlight>
-  );
-}
-
-function SignUpButton({
-  email,
-  username,
-  password,
-}: {
-  email: string;
-  username: string;
-  password: string;
-}) {
-  const mutation = useRegisterUser(() => {
+  const mutation = useRegister(() => {
     router.navigate("/");
   });
 
