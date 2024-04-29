9fa23065f7f9d66877923518bc2863bb6f35c7b7
Long Time No C <85569855+longtimeno-c@users.noreply.github.com>
2024-04-29 09:14:44 +0100

    added logs to login hook

--- hooks/api/useLogin.ts
+++ hooks/api/useLogin.ts
@@ -4,13 +4,28 @@ import { login } from "../../api/login";
 export default function useLogin() {
   const queryClient = useQueryClient();
   return useMutation({
-    mutationFn: ({
+    mutationFn: async ({
       username,
       password,
     }: {
       username: string;
       password: string;
-    }) => login(username, password),
+    }) => {
+      try {
+        const response = await login(username, password);
+        if (response.status === 200) {
+          console.log("Login successful");
+          alert("Login success!")
+        } else {
+          console.log("Invalid username or password");
+          alert("Invalid username or password")
+        }
+        return response;
+      } catch (error) {
+        console.error("Error occurred during login:", error);
+        throw error;
+      }
+    },
     onSuccess: () => {
       queryClient.invalidateQueries({
         queryKey: ["currentUser"],
