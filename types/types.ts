a8e7808d56ac944ac19e44c6b5b5d02fbf9655a3
Long Time No C <85569855+longtimeno-c@users.noreply.github.com>
2024-04-30 16:45:01 +0100

    added to missile library

--- types/types.ts
+++ types/types.ts
@@ -11,17 +11,38 @@ export interface Missilelib {
   quantity: number;
   description: string;
 }
-
-import nukeImage from '../assets/missiles/nuke.png';
-import cruiseImage from '../assets/missiles/cruise.png';
+//images
+import AmplifierImage from '../assets/missiles/Amplifier.png';
+import BallistaImage from '../assets/missiles/Ballista.png';
+import BigBerthaImage from '../assets/missiles/BigBertha.png';
+import BombabomImage from '../assets/missiles/Bombabom.png';
+import BunkerBlockerImage from '../assets/missiles/BunkerBlocker.png';
+import BuzzardImage from '../assets/missiles/Buzzard.png';
+import ClusterBombImage from '../assets/missiles/ClusterBomb.png';
+import CorporateRaiderImage from '../assets/missiles/CorporateRaider.png';
+import GutShotImage from '../assets/missiles/GutShot.png';
+import TheNukeImage from '../assets/missiles/TheNuke.png';
+import YokozunaImage from '../assets/missiles/Yokozuna.png';
+import ZippyImage from '../assets/missiles/Zippy.png';
 
 interface MissileImages {
-  [key: string]: any; // Index signature to allow any string key
+  [key: string]: any;
 }
 
 export const missileImages: MissileImages = {
-  nuke: nukeImage,
-  cruise: cruiseImage,
+  Amplifier: AmplifierImage,
+  Ballista: BallistaImage,
+  BigBertha: BigBerthaImage,
+  Bombabom: BombabomImage,
+  BunkerBlocker: BunkerBlockerImage,
+  Buzzard: BuzzardImage,
+  ClusterBomb: ClusterBombImage,
+  CorporateRaider: CorporateRaiderImage,
+  GutShot: GutShotImage,
+  TheNuke: TheNukeImage,
+  Yokozuna: YokozunaImage,
+  Zippy: ZippyImage,
+
   // Add other missile images here
 };
 
