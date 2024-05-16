f8e68ecdb4bcfd35699a7ae73766d0ed14a6780e
Kevin Gray <graykevinb@gmail.com>
2024-05-15 20:07:43 -0500

    Extract update fetching to map-comp.tsx

--- components/map-comp.tsx
+++ components/map-comp.tsx
@@ -1,17 +1,14 @@
-import React, { useState } from "react"
+import React, { useCallback, useEffect, useState } from "react"
 import MapView from "react-native-maps"
 import { AllLootDrops } from "./loot-drop";
 import { AllLandMines } from "./all-landmines";
 import { AllMissiles } from "./map-missile";
 import { AllPlayers } from "./all-players";
 import { Landmine, Loot, Missile } from "../types/types";
+import { fetchLootFromBackend, fetchMissilesFromBackend, fetchlandmineFromBackend } from "../temp/fetchMethods";
 
 interface MapCompProps {
-    lootLocations: Loot[];
-    missileData: Missile[];
-    landminedata: Landmine[];
     selectedMapStyle: any;
-
 }
 
 export const MapComp = (props: MapCompProps) => {
@@ -23,6 +20,38 @@ export const MapComp = (props: MapCompProps) => {
       };
       
     const [region, setRegion] = useState(defaultRegion); //null was defaultRegion but zoomed in ugly
+    const [lootLocations, setLootLocations] = useState<Loot[]>([]);
+    const [missileData, setMissileData] = useState<Missile[]>([]);
+    const [landminedata, setlandminelocations] = useState<Landmine[]>([]);
+
+    const fetchLootAndMissiles = useCallback(() => {
+        //fetch dist
+          const updateData = async () => {
+          const lootData = await fetchLootFromBackend();
+          const landminedata = await fetchlandmineFromBackend();
+          const missileData = await fetchMissilesFromBackend();
+    
+          setLootLocations(lootData);
+          setlandminelocations(landminedata);
+          setMissileData(missileData);
+        };
+    
+        // Initial fetch
+        updateData();
+    
+        // Fetch data every 30 seconds
+        const intervalId = setInterval(updateData, 30000); // 30 seconds
+    
+        // Cleanup interval on component unmount
+        return () => {
+          clearInterval(intervalId);
+        };
+      }, []);
+    
+      useEffect(() => {
+        fetchLootAndMissiles();
+      }, [fetchLootAndMissiles]);
+      
     return (
         <MapView
         // no longer supported -> provider={PROVIDER_GOOGLE}
@@ -33,13 +62,13 @@ export const MapComp = (props: MapCompProps) => {
         customMapStyle={props.selectedMapStyle} >
         
         {/* Render Loot Drops */}
-        <AllLootDrops lootLocations={props.lootLocations} />
+        <AllLootDrops lootLocations={lootLocations} />
 
         {/* Render landmine Drops */}
-        <AllLandMines landminedata={props.landminedata} />    
+        <AllLandMines landminedata={landminedata} />    
 
         {/* Render Missiles */}
-        <AllMissiles missileData={props.missileData} />
+        <AllMissiles missileData={missileData} />
         
         {/* Render Players */}
         <AllPlayers/>
