4b497cbca62f6444fb74dc9f5b7658a1759f1acf
Kevin Gray <graykevinb@gmail.com>
2024-05-15 20:07:43 -0500

    Extract update fetching to map-comp.tsx

--- app/map.tsx
+++ app/map.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useCallback } from "react";
+import React, { useState } from "react";
 import { View,  Platform } from "react-native";
 
 //Themes
@@ -8,15 +8,11 @@ import { CherryBlossomMapStyle } from "../themes/cherryBlossomMapStyle";
 import { CyberpunkMapStyle } from "../themes/cyberpunkstyle";
 import { ColorblindMapStyle } from "../themes/colourblindstyle";
 
-//Types
-import { Loot, Missile, Landmine } from "../types/types";
-
 //Components:
 import { MapStylePopup } from "../components/map-style-popup";
 import { getStoredMapStyle, storeMapStyle} from "../components/ui/mapthemestore"; //cache map theme 
 
 //Hooks
-import { fetchMissilesFromBackend, fetchLootFromBackend, fetchlandmineFromBackend } from "../temp/fetchMethods";
 import { ThemeSelectButton } from "../components/theme-select-button";
 import { FireSelector } from "../components/fire-selector";
 import { MapComp } from "../components/map-comp";
@@ -26,37 +22,6 @@ export default function Map() {
 
   const [selectedMapStyle, setSelectedMapStyle] = useState(DefaultMapStyle);
   const [ThemepopupVisible, setThemePopupVisible] = useState(false);
-  const [lootLocations, setLootLocations] = useState<Loot[]>([]);
-  const [missileData, setMissileData] = useState<Missile[]>([]);
-  const [landminedata, setlandminelocations] = useState<Landmine[]>([]);
-  
-  const fetchLootAndMissiles = useCallback(() => {
-    //fetch dist
-      const updateData = async () => {
-      const lootData = await fetchLootFromBackend();
-      const landminedata = await fetchlandmineFromBackend();
-      const missileData = await fetchMissilesFromBackend();
-
-      setLootLocations(lootData);
-      setlandminelocations(landminedata);
-      setMissileData(missileData);
-    };
-
-    // Initial fetch
-    updateData();
-
-    // Fetch data every 30 seconds
-    const intervalId = setInterval(updateData, 30000); // 30 seconds
-
-    // Cleanup interval on component unmount
-    return () => {
-      clearInterval(intervalId);
-    };
-  }, []);
-
-  useEffect(() => {
-    fetchLootAndMissiles();
-  }, [fetchLootAndMissiles]);
 
   //To allow player to upload their own this is modular
 
@@ -97,7 +62,7 @@ export default function Map() {
   return (
     <View className="flex-1 bg-gray-200">
       {/* The Actual Map where the magic happens */}
-      <MapComp lootLocations={lootLocations} missileData={missileData} landminedata={landminedata} selectedMapStyle={selectedMapStyle} />
+      <MapComp selectedMapStyle={selectedMapStyle} />
 
       {/* Dropdown button */}
       {Platform.OS === 'android' && (
