733e1c6def4ddd0a4bea526c911db73429dd2c7c
Your Name <your@email.com>
2026-06-02 20:55:59 +0100

    patched import

--- components/Landmine/map-landmines.tsx
+++ components/Landmine/map-landmines.tsx
@@ -7,7 +7,7 @@ import { convertimestampfuture } from "../../util/get-time-difference";
 import { useLandmine } from "../../util/Context/landminecontext";
 import { getImages } from "../../api/store";
 import { getLeagueAirspace } from "../player";
-import { useUserLeague } from "../../hooks/api/useUserLEague";
+import { useUserLeague } from "../../hooks/api/useUserLeague";
 import { calculateDistance, getCurrentLocation, location } from "../../util/locationreq";
 
 interface AllLandmineProps {
